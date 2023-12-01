dbf8e0de050a8382b30887711238db0e855d1777
hammadiqbal123 <152419372+hammadiqbal123@users.noreply.github.com>
2023-12-01 12:42:48 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+ var a = parseFloat(prompt("Enter marks for Math:"));
+var b = parseFloat(prompt("Enter marks for Sindhi:"));
+var c = parseFloat(prompt("Enter marks for Physics:"));
+var d = parseFloat(prompt("Enter marks for Computer:"));
+var e = parseFloat(prompt("Enter marks for Chemistry:"));
+var obtainMarks = a +b + c + d + e ;
+alert(obtainMarks);
